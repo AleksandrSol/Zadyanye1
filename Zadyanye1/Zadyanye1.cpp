0df62cfc9dfd8a22805b856ffbdb5c155ee0a044
AleksandrSol <66996293+AleksandrSol@users.noreply.github.com>
2020-06-17 01:33:47 +0300

    Update Zadyanye1.cpp

--- Zadyanye1/Zadyanye1.cpp
+++ Zadyanye1/Zadyanye1.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 void main()
 {
+    setlocale(LC_ALL, "Ukrainian");
+    cout << "IПЗI 18К\n";
+    cout << "Соляной Олександр Олегович\n";
+    cout << "Одновимiрнi масиви\n";
+    cout << "номер завдання 1\n";
     int mas[10] = { 1,10,2,20,3,30,4,40,5,50 };
     for (int i = 0; i < 10; i += 2)
     {
