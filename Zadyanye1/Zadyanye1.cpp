6d4d594409fb0fd29dfcb40ae45728324d7c8b1a
AleksandrSol <66996293+AleksandrSol@users.noreply.github.com>
2020-06-17 00:24:07 +0300

    Добавьте файлы проекта.

---
+++ Zadyanye1/Zadyanye1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+    int mas[10] = { 1,10,2,20,3,30,4,40,5,50 };
+    for (int i = 0; i < 10; i += 2)
+    {
+        cout << mas[i] << '\t' << mas[i + 1] << '\n';
+
+    }
+    system("pause>>void");
+}
